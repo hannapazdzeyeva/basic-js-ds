3414550cd586a54143898a6231faba7343dff409
hannapazdzeyeva <hanna.pazdzeyeva@gmail.com>
2023-12-13 14:52:58 +0300

    feat: root, add, has, find

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
@@ -8,24 +8,43 @@ const { NotImplementedError } = require('../extensions/index.js');
 */
 class BinarySearchTree {
 
+  constructor() {
+    this.rootTree = null
+  }
+
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.rootTree
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addNode(node, data) {
+    if (!node) return new Node(data)
+    if (node.data == data) return data
+    data < node.data ? node.left = this.addNode(node.left, data) : node.right = this.addNode(node.right, data)
+    return node
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    this.rootTree = this.addNode(this.rootTree, data)
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  searchNode(node, data) {
+    if (!node) return false
+    if (node.data == data) return true
+    return data < node.data ? this.searchNode(node.left, data) : this.searchNode(node.right, data)
+  }
+
+  has(data) {
+    return this.searchNode(this.rootTree, data)
+  }
+
+  returnNode(node, data) {
+    if (!node) return null
+    if (node.data == data) return node
+    return data < node.data ? this.returnNode(node.left, data) : this.returnNode(node.right, data)
+  }
+
+  find(data) {
+    return this.returnNode(this.rootTree, data)
   }
 
   remove(/* data */) {
